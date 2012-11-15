c06dfb6e4296f16aca5cd7245d64eaf5f1011990
joamag <joamag@gmail.com>
2012-11-15 15:42:22 +0000

    first commit

---
+++ Omni Timeline/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Omni Timeline
+//
+//  Created by Administrator on 11/15/12.
+//  Copyright (c) 2012 Hive Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Omni Timeline/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Omni Timeline
+//
+//  Created by Administrator on 11/15/12.
+//  Copyright (c) 2012 Hive Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
